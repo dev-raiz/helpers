d0350a27d0d3486be1a8f1c8045976622c4e7c64
Emerson Silveira <emerson_m_silveira@hotmail.com>
2023-08-22 13:51:36 -0300

    Correção de bug

--- src/ResponseTrait.php
+++ src/ResponseTrait.php
@@ -6,6 +6,7 @@ trait ResponseTrait
 {
     public function send(array $body, int $httpCode = 200): void
     {
+        header("Content-type: application/json; charset=utf-8");
         http_response_code($httpCode);
         echo json_encode($body);
         exit;
