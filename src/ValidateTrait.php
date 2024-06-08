af3fde14fb841e1a68efc307166c1179daa47f9c
Emerson Silveira <emerson_m_silveira@hotmail.com>
2024-06-08 10:06:09 -0300

    Trait de validação adicionado

---
+++ src/ValidateTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace devraiz;
+
+trait ValidateTrait
+{
+    public function validateThirteenCharactersToPhoneNumber(string $phone): string
+    {
+        $countryCode = (int) substr($phone, 0, 2); // Código do país
+
+        $newPhone = $phone;
+
+        if (in_array($countryCode, array(55))) {
+            $phoneLength = strlen($phone);
+
+            if ($phoneLength < 13) {
+                $part1 = substr($phone, 0, 4);
+                $part2 = substr($phone, 4, $phoneLength);
+
+                $newPhone = $part1 . '9' . $part2;
+            }
+
+            $newPhone = $phone;
+        }
+        
+        return $newPhone;
+    }
+}
